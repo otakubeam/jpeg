328716483f75ce9861cf0f629d7954c914a45b29
Andrew Orazov <andrew.orazov@gmail.com>
2024-11-13 16:58:43 +0300

    feat: zigzag iterator

--- main.cpp
+++ main.cpp
@@ -1,10 +1,61 @@
 #include <iostream>
+#include <array>
+#include <vector>
+#include <utility>
+
 
 constexpr double Pi = 3.14159265358979323846;
 
 struct Block {
     double pixels[8][8];
+
+  class ZigZagIterator {
+  public:
+    ZigZagIterator(Block& blk__, std::size_t index__ = 0): blk{blk__}, index(index__) {}
+
+    typedef double      value_type;
+    typedef value_type& reference;
+    typedef value_type* pointer;
+
+    // Dereference Operators
+    pointer operator->() const { 
+        auto [row, col] = zigzagOrder[index];
+        return &blk.pixels[row][col]; 
+    }
+    reference operator*() const { return *operator->(); }
+
+    // Pre-increment
+    ZigZagIterator& operator++() { ++index; return *this; }
+
+    bool operator==(ZigZagIterator right) const { return &right.blk == &blk && right.index == index; }
+    bool operator!=(ZigZagIterator right) const { return !operator==(right); }
+
+    reference operator[](std::size_t idx) {
+        auto [row, col] = zigzagOrder[idx];
+        return blk.pixels[row][col];
+    }
+
+  private:
+    Block& blk;
+    std::size_t index;
+
+    static constexpr std::array<std::pair<int, int>, 64> zigzagOrder = {
+        {{0, 0}, {0, 1}, {1, 0}, {2, 0}, {1, 1}, {0, 2}, {0, 3}, {1, 2},
+         {2, 1}, {3, 0}, {4, 0}, {3, 1}, {2, 2}, {1, 3}, {0, 4}, {0, 5},
+         {1, 4}, {2, 3}, {3, 2}, {4, 1}, {5, 0}, {6, 0}, {5, 1}, {4, 2},
+         {3, 3}, {2, 4}, {1, 5}, {0, 6}, {0, 7}, {1, 6}, {2, 5}, {3, 4},
+         {4, 3}, {5, 2}, {6, 1}, {7, 0}, {7, 1}, {6, 2}, {5, 3}, {4, 4},
+         {3, 5}, {2, 6}, {1, 7}, {2, 7}, {3, 6}, {4, 5}, {5, 4}, {6, 3},
+         {7, 2}, {7, 3}, {6, 4}, {5, 5}, {4, 6}, {3, 7}, {4, 7}, {5, 6},
+         {6, 5}, {7, 4}, {7, 5}, {6, 6}, {5, 7}, {6, 7}, {7, 6}, {7, 7}}};
+  };
+
+  
+    ZigZagIterator begin() { return ZigZagIterator(*this); }
+    ZigZagIterator end() { return ZigZagIterator(*this, 64); }
+
 };
+
 
 constexpr Block luminanceQuantTable = {{
     {16, 11, 10, 16, 24, 40, 51, 61},
