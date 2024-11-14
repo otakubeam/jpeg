363dd102761fdf2ec295e2b3a54b83bd065bb5d5
Andrew Orazov <andrew.orazov@gmail.com>
2024-11-14 12:30:30 +0300

    fix: whitespace

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ struct Block {
 
     template<bool IsConst>
     class ZigZagIteratorImpl {
-  public:
+    public:
         using BlockType = typename std::conditional<IsConst, const Block, Block>::type;
         using value_type = double;
         using reference = typename std::conditional<IsConst, const value_type&, value_type&>::type;
@@ -35,14 +35,14 @@ struct Block {
             typename std::enable_if<IsConst || !WasConst>::type* = nullptr)
             : blk(other.blk), index(other.index) {}
 
-    // Dereference Operators
-    pointer operator->() const { 
-        auto [row, col] = zigzagOrder[index];
-        return &blk.pixels[row][col]; 
-    }
-    reference operator*() const { return *operator->(); }
+        // Dereference Operators
+        pointer operator->() const {
+            auto [row, col] = zigzagOrder[index];
+            return &blk.pixels[row][col];
+        }
+        reference operator*() const { return *operator->(); }
 
-    // Pre-increment
+        // Pre-increment
         ZigZagIteratorImpl& operator++() { ++index; return *this; }
 
         // Post-increment
@@ -63,32 +63,32 @@ struct Block {
         }
 
         reference operator[](std::size_t idx) const {
-        auto [row, col] = zigzagOrder[idx];
-        return blk.pixels[row][col];
-    }
+            auto [row, col] = zigzagOrder[idx];
+            return blk.pixels[row][col];
+        }
 
-  private:
+    private:
         BlockType& blk;
-    std::size_t index;
+        std::size_t index;
 
-    static constexpr std::array<std::pair<int, int>, 64> zigzagOrder = {
-        {{0, 0}, {0, 1}, {1, 0}, {2, 0}, {1, 1}, {0, 2}, {0, 3}, {1, 2},
-         {2, 1}, {3, 0}, {4, 0}, {3, 1}, {2, 2}, {1, 3}, {0, 4}, {0, 5},
-         {1, 4}, {2, 3}, {3, 2}, {4, 1}, {5, 0}, {6, 0}, {5, 1}, {4, 2},
-         {3, 3}, {2, 4}, {1, 5}, {0, 6}, {0, 7}, {1, 6}, {2, 5}, {3, 4},
-         {4, 3}, {5, 2}, {6, 1}, {7, 0}, {7, 1}, {6, 2}, {5, 3}, {4, 4},
-         {3, 5}, {2, 6}, {1, 7}, {2, 7}, {3, 6}, {4, 5}, {5, 4}, {6, 3},
-         {7, 2}, {7, 3}, {6, 4}, {5, 5}, {4, 6}, {3, 7}, {4, 7}, {5, 6},
-         {6, 5}, {7, 4}, {7, 5}, {6, 6}, {5, 7}, {6, 7}, {7, 6}, {7, 7}}};
+        static constexpr std::array<std::pair<int, int>, 64> zigzagOrder = {
+            {{0, 0}, {0, 1}, {1, 0}, {2, 0}, {1, 1}, {0, 2}, {0, 3}, {1, 2},
+             {2, 1}, {3, 0}, {4, 0}, {3, 1}, {2, 2}, {1, 3}, {0, 4}, {0, 5},
+             {1, 4}, {2, 3}, {3, 2}, {4, 1}, {5, 0}, {6, 0}, {5, 1}, {4, 2},
+             {3, 3}, {2, 4}, {1, 5}, {0, 6}, {0, 7}, {1, 6}, {2, 5}, {3, 4},
+             {4, 3}, {5, 2}, {6, 1}, {7, 0}, {7, 1}, {6, 2}, {5, 3}, {4, 4},
+             {3, 5}, {2, 6}, {1, 7}, {2, 7}, {3, 6}, {4, 5}, {5, 4}, {6, 3},
+             {7, 2}, {7, 3}, {6, 4}, {5, 5}, {4, 6}, {3, 7}, {4, 7}, {5, 6},
+             {6, 5}, {7, 4}, {7, 5}, {6, 6}, {5, 7}, {6, 7}, {7, 6}, {7, 7}}};
 
         // Make other instantiations friends
         template<bool> friend class ZigZagIteratorImpl;
-  };
+    };
 
     // Non-const iteration
     iterator begin() { return iterator(*this); }
     iterator end() { return iterator(*this, 64); }
-  
+
     // Const iteration
     const_iterator begin() const { return const_iterator(*this); }
     const_iterator end() const { return const_iterator(*this, 64); }
